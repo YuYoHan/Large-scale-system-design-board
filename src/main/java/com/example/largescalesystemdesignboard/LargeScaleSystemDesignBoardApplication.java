2c25b93eaae1140703f78c4e041e32fc9676939a
YuYoHan <zxzz8014@naver.com>
2025-03-21 14:18:42 +0900

    [Add] 프로젝트 세팅

---
+++ src/main/java/com/example/largescalesystemdesignboard/LargeScaleSystemDesignBoardApplication.java
@@ -0,0 +1,13 @@
+package com.example.largescalesystemdesignboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LargeScaleSystemDesignBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LargeScaleSystemDesignBoardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/largescalesystemdesignboard/LargeScaleSystemDesignBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.largescalesystemdesignboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LargeScaleSystemDesignBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
