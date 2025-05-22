4f7dd2f1cca5a3f4244bfc80925f78f25d6bedb5
YuYoHan <zxzz8014@naver.com>
2025-05-22 15:13:29 +0900

    [Study] 작업 세팅

---
+++ service/article-read/src/main/java/com/example/service/Article-readApplication.java
@@ -0,0 +1,13 @@
+package com.example.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Article-readApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceApplication.class, args);
+    }
+
+}
